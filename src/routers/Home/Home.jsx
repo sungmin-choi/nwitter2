42fc18fb123c602b8e7eda2cb86fea5890ae85e8
sungmin-choi <namja306@naver.com>
2021-12-03 16:55:17 +0900

    Feat)Home 화면 input 기능 생성

--- src/routers/Home/Home.jsx
+++ src/routers/Home/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import styles from './Home.module.css';
 const Home = (props) => {
+    const [nweet,setNweet] = useState("");
+    
+    const onChange = (event) =>{
+        const {target:{value}} = event;
+        setNweet(value); 
+    }
+
+    const onSubmit =(event)=>{
+        event.preventDefault();
+        setNweet("");
+    }
+
     return(
-        <h1>Home</h1>
+        <div className={styles.container}>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={nweet} type="text" placeholder="input your mind" />
+            <input type="submit" value="send" />
+        </form>
+        </div>
     )
 };
 
